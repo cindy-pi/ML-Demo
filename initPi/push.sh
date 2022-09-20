f5f51b54adea0d9fca97cabce22edc3a5454a3db
Cindy Sullivan <cindy.sullivan@skyloch.com>
2022-09-20 16:51:23 -0400

    Added fix to push.sh for multiple init commands

--- initPi/push.sh
+++ initPi/push.sh
@@ -1,4 +1,5 @@
 
+
 export VAR_SSIDNAME=$1
 export VAR_SSIDPASS=$2
 export VAR_SSH_PATH=$3
@@ -12,7 +13,15 @@ cp $VAR_SSH_PATH $VAR_BOOTVOL/authorized_keys
 cp ssh				$VAR_BOOTVOL
 cp unattended			$VAR_BOOTVOL
 
-sed -i 'bak' 's/init=\/usr\/lib\/raspi-config\/init_resize.sh/cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 init=\/bin\/bash -c "mount -t proc proc \/proc; mount -t sysfs sys \/sys; mount \/boot; source \/boot\/unattended" /' $VAR_BOOTVOL/cmdline.txt
+cat $VAR_BOOTVOL/cmdline.txt
+
+echo sed -i 'bak' 's/init=\/usr\/lib\/raspi-config\/init_resize.sh/cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 init=\/bin\/bash -c "mount -t proc proc \/proc; mount -t sysfs sys \/sys; mount \/boot; source \/boot\/unattended" /' $VAR_BOOTVOL/cmdline.txt
+
+sed -i 'bak' 's/init=.*/cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 init=\/bin\/bash -c "mount -t proc proc \/proc; mount -t sysfs sys \/sys; mount \/boot; source \/boot\/unattended" /' $VAR_BOOTVOL/cmdline.txt
+
+cat $VAR_BOOTVOL/cmdline.txt
+
+echo $VAR_BOOTVOL
 
 chmod 777 $VAR_BOOTVOL/firstboot.sh
 
