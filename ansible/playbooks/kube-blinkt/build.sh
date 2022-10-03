d57edfe5ee70939f080a4a58b0106586624fbcd3
Cindy Sullivan <cindy.sullivan@skyloch.com>
2022-10-03 15:09:54 -0400

    Fixed docker repo build

--- ansible/playbooks/kube-blinkt/build.sh
+++ ansible/playbooks/kube-blinkt/build.sh
@@ -1,13 +1,32 @@
+#!/bin/bash
+echo "Running Docker Check"
+. ~/.profile
+export PATH=/usr/sbin/:$PATH
+export dockerUp=`sudo systemctl status docker | grep "active (running)" | wc -l`
 
+if [ "$dockerUp" != "1" ] ; then
+  echo "Running Docker Reset" 
+  sudo rm -rf /etc/docker/daemon.json
+  sudo systemctl stop docker.socket
+  sudo systemctl stop docker.service
+  sudo service docker stop
+  sudo service docker start
+fi
 
+echo "Running Registry Check"
 export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
 export registryStatus=`docker ps | grep registry | wc -l`
 
 if [ "$registryStatus" != "1" ] ; then
+  echo "Running Registry Install"
+  export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
+  ifconfig eth0  | grep 'inet ' | awk '{print $2}'
+  ifconfig eth0
+  echo "Running Registry k8sManager :: $k8sManager"
   sudo sh -c 'echo { \"insecure-registries\":[\"'$k8sManager':5000\"] } > /etc/docker/daemon.json'
+  cat /etc/docker/daemon.json
   sudo systemctl stop docker.socket
   sudo systemctl stop docker.service
-  sleep 10
   sudo service docker stop
   sudo service docker start
   sleep 10
@@ -16,10 +35,11 @@ else
   echo Registry is already running
 fi
 
-
+echo "Building kube monitor"
 docker build -t kube-blinkt .
 docker tag kube-blinkt $k8sManager:5000/kube-blinkt:latest
 docker push $k8sManager:5000/kube-blinkt:latest
 
+echo "Deploying kube monitor"
 ./deploy-kube-blinkt.sh
 
