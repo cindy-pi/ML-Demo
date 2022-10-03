67325ebe4a81cd0b27bb60b431a3b7664d30ff9c
Cindy Sullivan <cindy.sullivan@skyloch.com>
2022-10-02 20:05:36 -0400

    Adding kube blint monitor

---
+++ ansible/playbooks/files/k3s_build_private_repo_yaml.sh
@@ -0,0 +1,17 @@
+
+export k3sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
+
+cat <<EOF > ./privateRepo.yaml
+mirrors:
+  manager.registry.private:
+    endpoint:
+      - "http://$k3sManager:5000"
+configs:
+  "manager.registry.private":
+    tls:
+      insecure_skip_verify: true
+EOF
+#sudo chmod go-r /etc/rancher/k3s/registries.yaml
+
+#sudo systemctl restart k3s
+#sudo systemctl restart k3s-agent
--- ansible/playbooks/kube-blinkt/build.sh
+++ ansible/playbooks/kube-blinkt/build.sh
@@ -1,8 +1,26 @@
 
-docker login
-docker build -t blinkt .
-docker tag blinkt cindyspirion/blinkt:test$1
-docker push cindyspirion/blinkt:test$1
-docker push tag blinkt pi05.local:5000/blinkt:test$1
-docker push pi05.local:5000/blinkt:test$1
+
+
+export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
+export registryStatus=`docker ps | grep registry | wc -l`
+
+if [ "$registryStatus" != "1" ] ; then
+  sudo sh -c 'echo { \"insecure-registries\":[\"'$k8sManager':5000\"] } > /etc/docker/daemon.json'
+  sudo systemctl stop docker.socket
+  sudo systemctl stop docker.service
+  sleep 10
+  sudo service docker stop
+  sudo service docker start
+  sleep 10
+  docker run -d -p 5000:5000 --restart=always --name registry registry
+else
+  echo Registry is already running
+fi
+
+
+docker build -t kube-blinkt .
+docker tag kube-blinkt $k8sManager:5000/kube-blinkt:latest
+docker push $k8sManager:5000/kube-blinkt:latest
+
+./deploy-kube-blinkt.sh
 
