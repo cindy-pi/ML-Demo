19bf07678ef694256e2b73acd260d870cf0e3d16
Cindy Sullivan <31637301+cindy-pi@users.noreply.github.com>
2022-12-15 18:27:38 -0500

    11 add blinkt kubectl monitor deployment to playbook (#14)
    * Adding kube blint monitor

--- ansible/playbooks/kube-blinkt/build.sh
+++ ansible/playbooks/kube-blinkt/build.sh
@@ -4,6 +4,8 @@ echo "Running Docker Check"
 export PATH=/usr/sbin/:$PATH
 export dockerUp=`sudo systemctl status docker | grep "active (running)" | wc -l`
 
+
+cd /home/pi/kube-blinkt
 if [ "$dockerUp" != "1" ] ; then
   echo "Running Docker Reset" 
   sudo rm -rf /etc/docker/daemon.json
@@ -18,28 +20,29 @@ export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
 export registryStatus=`docker ps | grep registry | wc -l`
 
 if [ "$registryStatus" != "1" ] ; then
-  echo "Running Registry Install"
-  export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
-  ifconfig eth0  | grep 'inet ' | awk '{print $2}'
-  ifconfig eth0
-  echo "Running Registry k8sManager :: $k8sManager"
+  sudo rm -rf /etc/docker/daemon.json
   sudo sh -c 'echo { \"insecure-registries\":[\"'$k8sManager':5000\"] } > /etc/docker/daemon.json'
-  cat /etc/docker/daemon.json
   sudo systemctl stop docker.socket
   sudo systemctl stop docker.service
-  sudo service docker stop
-  sudo service docker start
+  sleep 10
+  sudo systemctl start docker.socket
+  sudo systemctl start docker.service
+  sudo systemctl is-enabled docker
+  sudo  systemctl enable --now docker
   sleep 10
   docker run -d -p 5000:5000 --restart=always --name registry registry
 else
   echo Registry is already running
 fi
 
-echo "Building kube monitor"
 docker build -t kube-blinkt .
 docker tag kube-blinkt $k8sManager:5000/kube-blinkt:latest
 docker push $k8sManager:5000/kube-blinkt:latest
 
-echo "Deploying kube monitor"
+docker build -t kube-blinkt-clear -f ./Dockerfile.clear .
+docker tag kube-blinkt-clear $k8sManager:5000/kube-blinkt:clear
+docker push $k8sManager:5000/kube-blinkt:clear
 ./deploy-kube-blinkt.sh
+
+kubectl patch daemonset -n default blinkt -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"date\":\"`date +'%s'`\"}}}}}"
 
---
+++ ansible/playbooks/kube-blinkt/set-hosts.sh
@@ -0,0 +1,26 @@
+
+
+
+export k8sManager=`ifconfig eth0  | grep 'inet ' | awk '{print $2}'`
+export registryStatus=`docker ps | grep registry | wc -l`
+
+if [ "$registryStatus" != "1" ] ; then
+  sudo sh -c 'echo { \"insecure-registries\":[\"'$k8sManager':5000\"] } > /etc/docker/daemon.json'
+  sudo systemctl stop docker.socket
+  sudo systemctl stop docker.service
+  sleep 10
+  sudo service docker stop
+  sudo service docker start
+  sleep 10
+  docker run -d -p 5000:5000 --restart=always --name registry registry
+else
+  echo Registry is already running
+fi
+
+
+docker build -t kube-blinkt .
+docker tag kube-blinkt $k8sManager:5000/kube-blinkt:latest
+docker push $k8sManager:5000/kube-blinkt:latest
+
+./deploy-kube-blinkt.sh
+
