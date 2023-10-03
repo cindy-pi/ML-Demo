c71a67d103e9a99f69b1e57317fd652c89680700
Cindy Sullivan <cindy.sullivan@skyloch.com>
2023-10-03 08:55:36 -0400

    Saving work

--- ansible/playbooks/files/demo/deploy-demo-server.sh
+++ ansible/playbooks/files/demo/deploy-demo-server.sh
@@ -4,5 +4,5 @@ docker build -t demo-server .
 
 docker stop demo-server
 docker rm demo-server
-docker run -d --rm --name demo-server -p 5005:5000 -e KUBECONFIG=/etc/kubernetes/kube.config -v /home/pi/internal.kubeconfig:/etc/kubernetes/kube.config demo-server
+docker run -d --rm --name demo-server -p 0.0.0.0:5005:5000 -e KUBECONFIG=/etc/kubernetes/kube.config -v /home/pi/internal.kubeconfig:/etc/kubernetes/kube.config demo-server
 
